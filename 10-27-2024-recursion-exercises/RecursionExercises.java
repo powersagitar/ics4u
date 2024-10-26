f274747e392a67d27855e260ec86bcba405146e6
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-10-26 13:33:28 -0400

    Add javadoc comments

--- 10-27-2024-recursion-exercises/RecursionExercises.java
+++ 10-27-2024-recursion-exercises/RecursionExercises.java
@@ -13,13 +13,19 @@ public class RecursionExercises {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // System.out.println(factorial(10));
+        System.out.println(factorial(10));
 
-        // gcf();
+        gcf();
 
         pow();
     }
 
+    /**
+     * Calculate factorial of given integer.
+     *
+     * @param x the number whose factorial is to be calculated
+     * @return factorial of {@code x}
+     */
     static int factorial(final int x) {
         assert x >= 0;
 
@@ -30,6 +36,10 @@ public class RecursionExercises {
         return x * factorial(x - 1);
     }
 
+    /**
+     * Prompts the user to enter 2 integer values, finds the greatest common factor,
+     * and displays the value.
+     */
     static void gcf() {
         System.out.println("Please enter 2 positive integers");
 
@@ -44,6 +54,14 @@ public class RecursionExercises {
         System.out.println("The greatest common factor of " + m + " and " + n + " is: " + _gcf(m, n));
     }
 
+    /**
+     * Finds the greatest common factor of {@code m} and {@code n}. {@code gcf()}
+     * calls this method under the hood.
+     *
+     * @param m a number whose greatest common factor is to be found
+     * @param n a number whose greatest common factor is to be found
+     * @return
+     */
     static int _gcf(final int m, final int n) {
         if (m == n) {
             return m;
@@ -54,6 +72,10 @@ public class RecursionExercises {
         }
     }
 
+    /**
+     * Prompts the user to enter a base and an exponent, calculates the base raised
+     * to the power of exponent, and displays the result.
+     */
     static void pow() {
         System.out.println("Please enter a real number as base and an integer as exponent.");
 
@@ -65,6 +87,13 @@ public class RecursionExercises {
         System.out.println(base + " raised to the power of " + exp + " is: " + dPower);
     }
 
+    /**
+     * Calculates the result of {@code base} raised to the power of {@code exp}.
+     *
+     * @param base the base number
+     * @param exp  the exponent
+     * @return the result of base raised to the power of exp
+     */
     static double _pow(final double base, final int exp) {
         if (exp == 0) {
             return 1;
