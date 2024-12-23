89cec0a4acd1b03562dbf419f0716f72376572a5
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-12-23 16:23:16 -0500

    Refactor `GameModeSelector`

--- culminating-mastermind/src/mastermind/gui/scenes/GameModeSelector.java
+++ culminating-mastermind/src/mastermind/gui/scenes/GameModeSelector.java
@@ -1,62 +1,49 @@
 package src.mastermind.gui.scenes;
 
-import src.mastermind.utils.Tuple2;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class GameModeSelector extends Scene {
+    private final JPanel selectorPanel = new JPanel();
+    private final JRadioButton codeBreakerButton = new JRadioButton("Code Breaker", true);
+    private final JRadioButton codeMakerButton = new JRadioButton("Code Maker");
+    private final JButton proceedButton = new JButton("Proceed");
+
     public GameModeSelector(final JFrame frame) {
         super(frame);
 
-        final JPanel selectorPanel = makeSelectorPanel();
+        drawSelectorPanel();
+        drawProceedButton();
+
         frame.add(selectorPanel);
-
-        final Tuple2<JRadioButton, JRadioButton> gameModeRadioButtons = makeGameModeRadioButtons();
-        final JRadioButton codeBreakerButton = gameModeRadioButtons.first;
-        final JRadioButton codeMakerButton = gameModeRadioButtons.second;
-
-        selectorPanel.add(codeBreakerButton);
-        selectorPanel.add(codeMakerButton);
-
-        final JButton proceedButton = makeProceedButton(codeBreakerButton, codeMakerButton);
         frame.add(proceedButton);
 
         refreshFrame();
     }
 
-    private JPanel makeSelectorPanel() {
-        final JPanel selectorPanel = new JPanel();
-
+    private void drawSelectorPanel() {
         selectorPanel.setLayout(new BoxLayout(selectorPanel, BoxLayout.Y_AXIS));
         selectorPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         selectorPanel.setBorder(BorderFactory.createTitledBorder("Game Modes"));
 
-        return selectorPanel;
+        selectorPanel.add(codeBreakerButton);
+        selectorPanel.add(codeMakerButton);
+
+        final ButtonGroup gameModeButtonGroup = new ButtonGroup();
+        gameModeButtonGroup.add(codeBreakerButton);
+        gameModeButtonGroup.add(codeMakerButton);
     }
 
-    private Tuple2<JRadioButton, JRadioButton> makeGameModeRadioButtons() {
-        final JRadioButton codeBreakerButton = new JRadioButton("Code Breaker", true);
-        final JRadioButton codeMakerButton = new JRadioButton("Code Maker");
-
-        final ButtonGroup selectorButtonGroup = new ButtonGroup();
-        selectorButtonGroup.add(codeBreakerButton);
-        selectorButtonGroup.add(codeMakerButton);
-
-        return new Tuple2<>(codeBreakerButton, codeMakerButton);
-    }
-
-    private JButton makeProceedButton(final JRadioButton codeBreakerButton, final JRadioButton codeMakerButton) {
-        final JButton proceedButton = new JButton("Proceed");
+    private void drawProceedButton() {
         proceedButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         proceedButton.addActionListener(_ -> {
             if (codeBreakerButton.isSelected()) {
                 new CodeBreakerSelector(frame);
             } else if (codeMakerButton.isSelected()) {
                 new CodeMaker(frame);
+            } else {
+                throw new IllegalArgumentException("A game mode has to be selected.");
             }
         });
-
-        return proceedButton;
     }
 }
