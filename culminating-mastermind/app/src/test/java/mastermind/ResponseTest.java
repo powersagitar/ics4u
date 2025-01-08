b857da5bd42286aec94dc874b53a594b484f057a
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-01-08 16:24:28 -0500

    Add more `Response` unit tests

--- culminating-mastermind/app/src/test/java/mastermind/ResponseTest.java
+++ culminating-mastermind/app/src/test/java/mastermind/ResponseTest.java
@@ -2,11 +2,13 @@ package mastermind;
 
 import mastermind.core.Code;
 import mastermind.core.Response;
+import mastermind.utils.Tuple2;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class ResponseTest {
     @Test
@@ -91,5 +93,33 @@ class ResponseTest {
 
         assertEquals(expectedCorrectCount, response.getResponse().first);
         assertEquals(expectedMisplacementCount, response.getResponse().second);
+    }
+
+    @Test
+    void responseFromInt() {
+        final Tuple2<Integer, Integer> counts = new Tuple2<>(1, 2);
+        final Response response = new Response(counts);
+
+        assertEquals(counts, response.getResponse());
+    }
+
+    @Test
+    void equals() {
+        final Response response1 = new Response(new Tuple2<>(1, 2));
+        final Response response2 = new Response(new Tuple2<>(1, 2));
+        final Response response3 = new Response(new Tuple2<>(2, 1));
+
+        assertEquals(response1, response2);
+        assertEquals(response2, response1);
+        assertEquals(response1, response1);
+        assertEquals(response2, response2);
+        assertEquals(response1.hashCode(), response2.hashCode());
+
+        assertNotEquals(response1, response3);
+        assertNotEquals(response2, response3);
+        assertNotEquals(response3, response1);
+        assertNotEquals(response3, response2);
+        assertNotEquals(response1.hashCode(), response3.hashCode());
+        assertNotEquals(response2.hashCode(), response3.hashCode());
     }
 }
