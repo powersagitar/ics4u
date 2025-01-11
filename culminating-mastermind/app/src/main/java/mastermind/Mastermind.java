3acceeff8c6866b7009c7fe8b0e9570921fddaaf
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-01-11 10:25:53 -0500

    Refactor logger initialization

--- culminating-mastermind/app/src/main/java/mastermind/Mastermind.java
+++ culminating-mastermind/app/src/main/java/mastermind/Mastermind.java
@@ -16,17 +16,23 @@ public class Mastermind {
     public final static Dimension CANVAS_DIMENSION = new Dimension(640, 480);
     public final static Logger log = new Logger();
 
-    static {
+    private static void initializeLogger() {
         try {
             final FileOutputStream logFile = new FileOutputStream("mastermind.log", false);
             log.addSink(logFile);
         } catch (final FileNotFoundException e) {
-            log.error("Failed to open log file: " + e.getMessage());
+            log.error("Failed to open log file output stream: " + e.getMessage());
         }
     }
 
     public static void main(String[] args) {
+        initializeLogger();
+
         log.info("Starting Mastermind");
+        log.debug("Total colors: " + TOTAL_COLORS);
+        log.debug("Code length: " + CODE_LENGTH);
+        log.debug("Max guesses: " + MAX_GUESSES);
+        log.debug("Canvas dimension: " + CANVAS_DIMENSION);
 
         final JFrame frame = Scene.createDefaultScene();
         new GameModeSelector(frame);
