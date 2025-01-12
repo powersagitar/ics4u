6f37f69d37007ddd5361fef7e3c82239d6187236
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-01-12 15:06:48 -0500

    Add javadoc

--- culminating-mastermind/app/src/main/java/mastermind/core/solvers/HumanSolver.java
+++ culminating-mastermind/app/src/main/java/mastermind/core/solvers/HumanSolver.java
@@ -5,7 +5,13 @@ import mastermind.core.Code;
 import mastermind.core.Response;
 import mastermind.utils.Tuple2;
 
+/**
+ * A solver that allows a human player to guess the secret code.
+ */
 public class HumanSolver extends MastermindSolver {
+    /**
+     * The secret code to be guessed during the game.
+     */
     private final Code secretCode;
 
     /**
@@ -25,21 +31,19 @@ public class HumanSolver extends MastermindSolver {
      * correctness of the guess in terms of the secret code. Based on the number
      * of correct matches and the player's remaining attempts, the method returns
      * a tuple containing the game status and a response object.
-     * </p>
      *
      * <p>
      * The game status can be one of the following:
      * - {@code Status.Win}: If the number of correct matches equals the code length.
      * - {@code Status.Lose}: If the maximum number of guesses is reached without success.
      * - {@code Status.Continue}: If the game is ongoing and further attempts are allowed.
-     * </p>
      *
      * @param guess the code guessed by the player
      * @return a tuple containing the game status and the response
      */
     public Tuple2<Status, Response> guess(final Code guess) {
-        // isLosing is mainly to increment the attempt counter here
-        // the losing status is first indicated by
+        // hasExceededMaxGuesses is mainly to increment the attempt counter here
+        // the status is first indicated by
         // getAttempts() == Mastermind.MAX_GUESSES
         if (hasExceededMaxGuesses()) {
             return new Tuple2<>(Status.Lose, null);
