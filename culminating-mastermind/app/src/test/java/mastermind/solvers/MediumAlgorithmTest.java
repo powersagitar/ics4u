b582de16746748e9d69a82b605801d9415139fec
Kenneth Chen <137091914+kennethchen416@users.noreply.github.com>
2025-01-14 17:18:19 -0500

    create MediumAlgorithmTest.java

---
+++ culminating-mastermind/app/src/test/java/mastermind/solvers/MediumAlgorithmTest.java
@@ -0,0 +1,31 @@
+package mastermind.solvers;
+
+import mastermind.core.Code;
+import mastermind.core.CodeFactory;
+import mastermind.core.Response;
+import mastermind.core.solvers.MediumAlgorithm;
+import mastermind.core.solvers.Status;
+import mastermind.utils.Tuple2;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MediumAlgorithmTest {
+    @Test
+    void testFirstThreeGuesses() {
+        final MediumAlgorithm solver = new MediumAlgorithm();
+        for (int i = 0; i < 3; i++) {
+            final Code firstGuess = solver.guess();
+            final Code expectedIthGuess =
+                CodeFactory.fromColorIndices(List.of(i, i, i, i));
+            assertEquals(expectedIthGuess.getColors(), firstGuess.getColors());
+        }
+    }
+
+    @Test
+    void testGuesses(){
+
+    }
+}
