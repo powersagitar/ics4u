6f2ea474c20ad4bcf5e3f9acc43f8a9367c4ac99
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-01-14 19:33:13 -0500

    add explicit jupiter version number

--- culminating-mastermind/app/build.gradle.kts
+++ culminating-mastermind/app/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     // Use JUnit Jupiter for testing.
-    testImplementation(libs.junit.jupiter)
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
