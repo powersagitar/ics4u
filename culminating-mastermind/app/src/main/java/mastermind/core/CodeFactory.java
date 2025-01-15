5755fb8fb59c50be3868a0a9fcbeb72643f57882
Kenneth Chen <137091914+kennethchen416@users.noreply.github.com>
2025-01-15 04:43:35 -0500

    add comments to CodeFactory

--- culminating-mastermind/app/src/main/java/mastermind/core/CodeFactory.java
+++ culminating-mastermind/app/src/main/java/mastermind/core/CodeFactory.java
@@ -17,6 +17,7 @@ public class CodeFactory {
      * Private constructor to prevent instantiation.
      */
     private CodeFactory() {
+        // Prevent instantiation of the factory class, throwing an exception.
         throw new IllegalStateException("Factory class should not be instantiated.");
     }
 
@@ -27,12 +28,14 @@ public class CodeFactory {
      * @return A {@link Code} object.
      */
     public static Code fromColorIndices(final List<Integer> indices) {
+        // Convert the list of color indices to a list of color objects.
+        // Stores in List colors
         final List<Code.Color> colors = indices
             .stream()
             .map(Code.Color::fromIndex)
             .toList();
 
-        return new Code(colors);
+        return new Code(colors); // Return a new Code object with the list of colors.
     }
 
     /**
@@ -41,15 +44,20 @@ public class CodeFactory {
      * @return A random {@link Code} object.
      */
     public static Code getRandom() {
+        // Create a random object.
         final Random random = new Random();
+        // Create a list of integers to store the color indices.
         final ArrayList<Integer> codeBuilder = new ArrayList<>(Mastermind.CODE_LENGTH);
 
+
+        // Generate a random color index and add it to the list of color indices.
         for (int i = 0; i < Mastermind.CODE_LENGTH; ++i) {
-            final int randColor = random.nextInt(Mastermind.TOTAL_COLORS);
+            final int randColor = random.nextInt(Mastermind.TOTAL_COLORS); //
+            // Generate a random color index, store in randColor
             codeBuilder.add(randColor);
         }
 
-        return fromColorIndices(codeBuilder);
+        return fromColorIndices(codeBuilder); // Return a new Code object with the list of color indices.
     }
 
     /**
@@ -63,42 +71,58 @@ public class CodeFactory {
      */
     @SuppressWarnings("ExtractMethodRecommender")
     public static Code getRandomFromFile(final File file)
+        // throws FileNotFoundException, IllegalArgumentException
         throws FileNotFoundException {
+        // Create a list of strings to store the lines of the file.
         final List<String> lines = new ArrayList<>();
+        // Create a scanner object to read the file.
         final Scanner scanner = new Scanner(file);
 
+        // Read the lines of the file and store them in the list of strings.
         while (scanner.hasNextLine()) {
             lines.add(scanner.nextLine());
         }
 
+        // Close the scanner.
         scanner.close();
 
+        // If the list of lines is empty, throw an exception.
         if (lines.isEmpty()) {
             throw new IllegalArgumentException("File is empty");
         }
 
+        // Create a random object.
         final Random random = new Random();
+        // Generate a random line number.
         final int randLineNo = random.nextInt(lines.size());
+        // Get the random line from the list of lines.
         final String randLine = lines.get(randLineNo);
 
+        // If the length of the random line is not equal to the code length, throw an exception.
         if (randLine.length() != Mastermind.CODE_LENGTH) {
             throw new IllegalArgumentException("Invalid code length");
         }
 
+        // Convert the random line to a character array.
         final char[] chars = randLine.toCharArray();
 
+        // Create a list of integers to store the color indices.
         final List<Integer> codeBuilder = new ArrayList<>(Mastermind.CODE_LENGTH);
 
+        // Iterate over the characters of the random line.
         for (final char c : chars) {
-            final int numericValue = Character.getNumericValue(c);
+            final int numericValue = Character.getNumericValue(c); // Get the numeric value of the character.
 
+            // If the numeric value is less than 0 or greater than or equal to the total number of colors, throw an exception.
             if (numericValue < 0 || numericValue >= Mastermind.TOTAL_COLORS) {
                 throw new IllegalArgumentException("Invalid color index");
             }
 
+            // Add the numeric value to the list of color indices.
             codeBuilder.add(numericValue);
         }
 
+        // Return a new Code object with the list of color indices.
         return fromColorIndices(codeBuilder);
     }
 }
